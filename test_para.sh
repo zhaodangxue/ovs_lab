c107ce1da1f640c454c125cf15c8faac63e912b2
zhaodangxue <z404583637@sjtu.edu.cn>
2024-07-31 12:13:55 +0000

    modify test_para .sh

--- test_para.sh
+++ test_para.sh
@@ -2,7 +2,8 @@
 file_name_1="./data/iperf_result/normal_para.txt"
 file_name_2="./data/iperf_result/numa_para.txt"
 file_name_3="./data/iperf_result/numa_diff_para.txt"
-para_nums=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32)
+#从1到64并行流
+para_nums=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64)
 mkdir -p "$(dirname "$file_name_1")"
 mkdir -p "$(dirname "$file_name_2")"
 mkdir -p "$(dirname "$file_name_3")"
